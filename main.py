3e39b4493c527e2283f2c9d0c4173a0451dcea90
anominiminiz <anoimiya369@gmail.com>
2021-03-10 12:26:18 +0900

    delete lines

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+# def handle_message(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text))
 
 # QuickReply追加部分
 # 何を送っても同じ反応をする
